152e488fe8504fa0422571faba5576ab16aac0d2
Baltasar Jiménez <bjimenez@nosolosoftware.es>
2019-05-17 14:50:43 +0200

    TTS añadido

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
+const {Client, RichEmbed} = require('discord.js');
 // const fs = require('fs');
 
-const client = new Discord.Client();
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -19,6 +19,17 @@ client.on('message', msg => {
       files: [{
         attachment: './images/tenor.gif',
         name: 'tenor.gif'
+      }]
+    });
+  }
+
+  if (msg.content === '!random') {
+    const number = Math.floor(Math.random() * 5) + 1;
+
+    msg.reply({
+      files: [{
+        attachment: `./images/${number}.png`,
+        name: `${number}.gif`
       }]
     });
   }
@@ -41,6 +52,21 @@ client.on('message', msg => {
 
   if (msg.content === '!leave') {
     msg.member.voiceChannel.leave();
+  }
+
+  if (msg.content === '!embed') {
+    const embed = new RichEmbed()
+      .setTitle('la puta madre')
+      .setColor(0xFF0000)
+      .setDescription('La puta madre pero descrita!');
+
+    msg.channel.send(embed);
+  }
+
+  if (msg.content === '!despacito') {
+    msg.channel.send('DESPACITO DESPACITO', {
+      tts: true
+    });
   }
 });
 
